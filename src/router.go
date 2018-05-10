d102cba90635f54f94e4197e6a599d446da77bfb
Maykonn <maykonn@outlook.com>
2018-05-10 18:20:07 -0300

    Router now supports public and private routes

--- src/router.go
+++ src/router.go
@@ -2,23 +2,41 @@ package src
 
 import (
 	"github.com/gorilla/mux"
-	"maykonn/api/src/handlers/default_http_verb"
 	"maykonn/api/src/handlers/authorization_token"
-	"net/http"
+	"maykonn/api/src/handlers/default_http_verb"
+	"maykonn/api/src/handlers/middleware"
 )
 
+const defaultVerbsRoutes = "/{route:[\\w\\d-]{1,}}"
+
 func Router() *mux.Router {
+	MainRouter := mux.NewRouter()
+	AuthorizationRouter := AuthorizationRouter()
+	DefaultVerbsRouter := DefaultVerbsRouter()
+
+	MainRouter.Handle("/authorization", AuthorizationRouter)
+	MainRouter.Handle(defaultVerbsRoutes, DefaultVerbsRouter)
+
+	return MainRouter
+}
+
+func AuthorizationRouter() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/authorization", authorization_token.New).Methods("GET")
+	return router
+}
 
-	const defaultRoute = "/{route:[\\w\\d-]{1,}}"
-	router.HandleFunc(defaultRoute, default_http_verb.Get).Methods("GET")
-	router.HandleFunc(defaultRoute, default_http_verb.Post).Methods("POST")
-	router.HandleFunc(defaultRoute, default_http_verb.Put).Methods("PUT")
-	router.HandleFunc(defaultRoute, default_http_verb.Patch).Methods("PATCH")
-	router.HandleFunc(defaultRoute, default_http_verb.Delete).Methods("DELETE")
+func DefaultVerbsRouter() *mux.Router {
+	router := mux.NewRouter()
 
-	http.Handle("/", router)
+	router.HandleFunc(defaultVerbsRoutes, default_http_verb.Get).Methods("GET")
+	router.HandleFunc(defaultVerbsRoutes, default_http_verb.Post).Methods("POST")
+	router.HandleFunc(defaultVerbsRoutes, default_http_verb.Put).Methods("PUT")
+	router.HandleFunc(defaultVerbsRoutes, default_http_verb.Patch).Methods("PATCH")
+	router.HandleFunc(defaultVerbsRoutes, default_http_verb.Delete).Methods("DELETE")
+
+	Authorization := middleware.Authorization{}
+	router.Use(Authorization.Middleware)
 
 	return router
 }
