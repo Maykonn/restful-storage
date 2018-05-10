a8851d954f42b086c053fd494798137ab19d368d
Maykonn <maykonn@outlook.com>
2018-05-10 17:37:57 -0300

    Program execution now creates a .pid file to use with make stop, to force stop the process by pid

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"maykonn/api/src"
+	"io/ioutil"
+	"strconv"
+	"os"
+	"syscall"
+	"fmt"
 )
 
 func main() {
-	src.StartServer()
+	if err := godotenv.Load(); err == nil {
+		writePidFile("./tmp/.pid")
+		src.StartServer()
+	}
+	panic("Error loading .env file")
+}
+
+// Write a pid file, but first make sure it doesn't exist with a running pid.
+// https://gist.github.com/davidnewhall/3627895a9fc8fa0affbd747183abca39
+func writePidFile(pidFile string) error {
+	// Read in the pid file as a slice of bytes.
+	if piddata, err := ioutil.ReadFile(pidFile); err == nil {
+		// Convert the file contents to an integer.
+		if pid, err := strconv.Atoi(string(piddata)); err == nil {
+			// Look for the pid in the process list.
+			if process, err := os.FindProcess(pid); err == nil {
+				// Send the process a signal zero kill.
+				if err := process.Signal(syscall.Signal(0)); err == nil {
+					// We only get an error if the pid isn't running, or it's not ours.
+					return fmt.Errorf("pid already running: %d", pid)
+				}
+			}
+		}
+	}
+	// If we get here, then the pidfile didn't exist,
+	// or the pid in it doesn't belong to the user running this app.
+	return ioutil.WriteFile(pidFile, []byte(fmt.Sprintf("%d", os.Getpid())), 0664)
 }
