2c116da2acdd5f9f574de7aebe1e35001601db9e
Maykonn <maykonn@outlook.com>
2018-05-11 17:28:14 -0300

    Refact config

--- src/server.go
+++ src/server.go
@@ -8,11 +8,8 @@ import (
 
 func StartServer() {
 	if address := os.Getenv("SERVER_ADDRESS"); address != "" {
-		if port := os.Getenv("SERVER_PORT"); port != "" {
-			address += ":" + port
-			log.Println("Server listening at ->", address)
-			panic(http.ListenAndServe(address, Router()))
-		}
+		log.Println("Server listening at ->", address)
+		panic(http.ListenAndServe(address, Router()))
 	}
-	panic("Server configuration is invalid")
+	panic("Invalid Http Server configuration")
 }
