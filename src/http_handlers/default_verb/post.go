f2cf56aec0a85276343768c96aa05a8d62c1dd8c
Maykonn <maykonn@outlook.com>
2018-05-11 18:25:22 -0300

    Calling repo layer.

--- src/http_handlers/default_verb/post.go
+++ src/http_handlers/default_verb/post.go
@@ -1,10 +1,13 @@
-package default_http_verb
+package default_verb
 
 import (
 	"net/http"
 	"fmt"
+	"maykonn/restful-storage/src/storage"
 )
 
 func Post(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Post called")
+
+	storage.Create(r.Body)
 }
